4277cf353c810261a464440571eb730994226a31
Lucas Dantas <142459897+lukazfivee@users.noreply.github.com>
2026-08-26 12:08:20 -0300

    chore: force redeploy services module cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='financeiro-eduardo-v4';
+const CACHE='financeiro-eduardo-v5';
 const ASSETS=['/','/index.html','/style.css','/services.css','/theme-dark.css','/theme.js','/app.js','/services.js','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
